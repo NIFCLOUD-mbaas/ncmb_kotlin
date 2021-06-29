ac5b2953f5bc21faa173fc1bf8b4f7af43561e0f
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2021-06-29 11:47:48 +0900

    Add licence header to srouce code

--- src/main/java/com/nifcloud/mbaas/core/NCMBDateFormat.kt
+++ src/main/java/com/nifcloud/mbaas/core/NCMBDateFormat.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
+ * Copyright 2017-2021 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.nifcloud.mbaas.core
 
 import java.text.SimpleDateFormat
