80892da98871eb02be1a48dcb860d6fc5e74b0cd
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2021-09-29 13:01:29 +0900

    Update import library for NCMBConnection

--- src/main/java/com/nifcloud/mbaas/core/NCMBConnection.kt
+++ src/main/java/com/nifcloud/mbaas/core/NCMBConnection.kt
@@ -20,9 +20,15 @@ import android.net.Uri
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import okhttp3.*
+import okhttp3.Headers
+import okhttp3.Callback
+import okhttp3.Call
+import okhttp3.Response
 import okhttp3.Headers.Companion.toHeaders
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 import java.io.IOException
 import java.net.URL
 import java.net.URLEncoder
