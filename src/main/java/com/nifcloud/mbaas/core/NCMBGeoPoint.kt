c9952c7de7e6e9fa277f8a9f0090b8c34737855d
watanabe-tomoya <tomoya.watanabe@fujitsu.com>
2021-10-25 15:23:25 +0900

    fix code

--- src/main/java/com/nifcloud/mbaas/core/NCMBGeoPoint.kt
+++ src/main/java/com/nifcloud/mbaas/core/NCMBGeoPoint.kt
@@ -1,33 +1,31 @@
 package com.nifcloud.mbaas.core;
 
-import java.lang.IllegalArgumentException
+class NCMBGeoPoint {
 
-//
-class NCMBGeoPoint(latitude : Double ,longitude : Double) {
+    var mlatitude: Double
+    var mlongitude: Double
 
     /// 例外処理
-    fun validate (latitude : Double, longitude : Double) : Pair<Double, Double>{
-        if(latitude !is Double || longitude !is Double){
-            throw IllegalArgumentException("GeoPoint latitude and longitude should be number")
+    @Throws(NCMBException::class)
+    fun validate(latitude: Double, longitude: Double): Pair<Double, Double> {
+        if (latitude < -90.0 || latitude > 90) {
+            throw NCMBException(
+                NCMBException.NOT_EFFICIENT_VALUE,
+                "set the latitude to a value between -90 and 90"
+            )
         }
-        if(latitude < -90.0 || latitude > 90){
-            throw IllegalArgumentException("set the latitude to a value between -90 and 90")
-        }
-        if(longitude < -180.0 || longitude > 180.0){
-            throw IllegalArgumentException("set the longitude to a value between -180 and 180")
+        if (longitude < -180.0 || longitude > 180.0) {
+            throw NCMBException(
+                NCMBException.NOT_EFFICIENT_VALUE,
+                "set the longitude to a value between -180 and 180"
+            )
         }
         return Pair(latitude, longitude)
     }
 
-    /// オブジェクト化処理
-    fun toObject (typename : String, latObject : String, lngObject : String){
-
-    }
-
-    /// メイン処理
-    fun geoPoint (lat : Double, lng : Double){
-        var position : Pair<Double, Double> = validate(lat, lng)
-        var GeoObject = toObject()
-        return GeoObject
+    constructor(latitude: Double = 0.0, longitude: Double = 0.0) {
+        val position = validate(latitude, longitude)
+        mlatitude = position.first
+        mlongitude = position.second
     }
 }
--- src/test/java/com/nifcloud/mbaas/core/NCMBGeoPointTest.kt
+++ src/test/java/com/nifcloud/mbaas/core/NCMBGeoPointTest.kt
@@ -8,20 +8,40 @@ class NCMBGeoPointTest {
     fun test_geopoint_right_settings(){
         val latitude : Double = 35.6666269
         val longitude : Double = 139.765607
-        val geopoint = geoPoint(latitude, longitude)
-        Assert.assertEquals(geopoint.latitude, 35.6666269)
-        Assert.assertEquals(geopoint.longitude, 139.765607)
+        val geopoint = NCMBGeoPoint(latitude, longitude)
+        Assert.assertEquals(geopoint.mlatitude, latitude, 0.0)
+        Assert.assertEquals(geopoint.mlongitude, longitude, 0.0)
     }
 
     @Test
     fun test_geopoint_init_settings(){
-        val geopoint = geoPoint()
-        Assert.assertEquals(geopoint.latitude, 0)
-        Assert.assertEquals(geopoint.longitude, 0)
+        val geopoint = NCMBGeoPoint()
+        Assert.assertEquals(geopoint.mlatitude, 0.0, 0.0)
+        Assert.assertEquals(geopoint.mlongitude, 0.0, 0.0)
     }
 
     @Test
     fun test_geopoint_wrong_latitude(){
-
+        try {
+            val latitude: Double = 139.765607
+            val longitude: Double = 139.765607
+            val geopoint = NCMBGeoPoint(latitude, longitude)
+        }
+        catch(e: NCMBException){
+            Assert.assertEquals(e.message, "set the latitude to a value between -90 and 90")
+        }
     }
+
+    @Test
+    fun test_geopoint_wrong_longitude(){
+        try {
+            val latitude: Double = 35.6666269
+            val longitude: Double = 189.765607
+            val geopoint = NCMBGeoPoint(latitude, longitude)
+        }
+        catch(e: NCMBException){
+            Assert.assertEquals(e.message, "set the longitude to a value between -180 and 180")
+        }
+    }
+
 }
